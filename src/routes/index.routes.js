6b7a676de4bee939063c1ad934a9f8f36d2d3d0e
up200512 <up200512@alumnos.upa.edu.mx>
2023-06-14 00:30:56 -0600

    Acabo de modificar el nuevo index_routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,7 @@ import { Router} from "express";
 import {ping } from '../controllers/index.controllers.js';
 const router = Router();
 router.get('/', (req, res)=>{
-    res.send("hello word en routes");
+    res.send("hello word en rout");
 });
 
 router.get('/ping',ping);
