cd4b0b51e1114019c61657907344450a35d21a1a
UP200423 <up200423@alumnos.upa.edu.mx>
2023-06-23 15:49:34 -0600

    servidor del 23/06/2023

--- src/controllers/insumos.controllers.js
+++ src/controllers/insumos.controllers.js
@@ -3,7 +3,7 @@ import { pool } from '../db.js'
 
 export const getInsumos = async (req, res) => {
     try {
-        const [rows] = await pool.query('select id_insumo, nombre, tipo_insumo_id, unidad_de_medida_id, descripcion, cantidad from insumos');
+        const [rows] = await pool.query('select id_insumo, i.nombre, i.tipo_insumo_id, t.nombre as nombre_tipo, i.unidad_de_medida_id, u.nombre as nombre_unidad,  u.abreviacion, descripcion, cantidad from insumos i inner join tipos_de_insumo t on i.tipo_insumo_id = t.id_tipo inner join  unidades_de_medida u on i.unidad_de_medida_id = u.id_unidad;');
         res.status(200).json(rows);
     } catch (error) {
         return res.status(500).json({
@@ -47,8 +47,10 @@ export const createInsumo = async (req, res) =>{
 export const deleteInsumo = async (req, res) =>{
     // console.log(req.params.id);
     const id = req.params.id;
+    let sql1= 'delete from detalle_de_productos where id_insumo= ?'
     let sql = 'delete from insumos where id_insumo = ?'
     try {
+        const [rows1] = await pool.query(sql1, [id])
         const [rows] = await pool.query(sql, [id])
         res.status(200).json(rows)
     } catch (error) {
