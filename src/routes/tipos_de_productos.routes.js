95f11e47429a45c8082b68cd58c453cb37d01554
UP200667 <up200667@alumnos.upa.edu.mx>
2023-07-19 21:26:38 -0600

    Servidor Actualizado

--- src/routes/tipos_de_productos.routes.js
+++ src/routes/tipos_de_productos.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getTiposProductos, createTiposProductos, deleteTiposProductos, updateTiposProductos, getTipoProductos} from '../controllers/tipos_de_producto.controllers.js';
+import { getTiposProductos, getTiposProductosMaxId, createTiposProductos, deleteTiposProductos, updateTiposProductos, getTipoProductos} from '../controllers/tipos_de_producto.controllers.js';
 
 const router = Router();
 
 router.get('/tiposProductos', getTiposProductos); //listo
-
+router.get('/tiposProductosMaxId', getTiposProductosMaxId); //listo
 router.get('/tiposProductos/:id', getTipoProductos); //listo
 router.get('/tiposProductos', getTiposProductos); //listo
 router.post('/tiposProductos', createTiposProductos); //listo
