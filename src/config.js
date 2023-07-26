e17353308f837842a01857e63af61cd366be1a3b
up200512 <up200512@alumnos.upa.edu.mx>
2023-07-25 23:21:40 -0600

    login, users, DBScript, Pedidos

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ import { config } from "dotenv"
 config()
 console.log (process.env.PORT)
 export const PORT = process.env.PORT || 3001
+export const SECRET= process.env.SECRET
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_PORT = process.env.DB_PORT || 3306
 export const DB_DATABASE = process.env.DB_DATABASE 
