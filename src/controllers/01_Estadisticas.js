f12ba78906365634c124a308dfb3bf5feac42431
UP200760 <113131310+UP200760@users.noreply.github.com>
2023-08-08 22:10:21 -0600

    cambios rutas
    10:10 pm

--- src/controllers/01_Estadisticas.js
+++ src/controllers/01_Estadisticas.js
@@ -1,8 +1,40 @@
 import { pool } from "../db.js";
+
+
 export const getLastMax = async (req, res) => {
   try {
     // const id=req.params.id;
     const [rows] = await pool.query("SELECT p.nombre AS nombre, p.precio as precio_actual, SUM(dp.cantidad) AS total_vendido, SUM(dp.cantidad * dp.precio_unitario) AS Ingresos FROM detalle_de_pedidos dp INNER JOIN productos_en_venta p ON dp.id_producto = p.id_producto WHERE dp.fecha >= DATE_SUB(NOW(), INTERVAL 2 WEEK) AND dp.fecha < NOW() group by nombre  order by total_vendido desc limit 5");
+    if (rows.length <= 0) {
+      return res.status(400).json({ message: "no encontrado" });
+    }
+    res.status(200).json(rows);
+  } catch (error) {
+    return res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+};
+
+export const getOneMax = async (req, res) => {
+  try {
+    // const id=req.params.id;
+    const [rows] = await pool.query("SELECT p.nombre AS nombre, p.precio as precio_actual, SUM(dp.cantidad) AS total_vendido, SUM(dp.cantidad * dp.precio_unitario) AS Ingresos FROM detalle_de_pedidos dp INNER JOIN productos_en_venta p ON dp.id_producto = p.id_producto WHERE dp.fecha >= DATE_SUB(NOW(), INTERVAL 2 WEEK) AND dp.fecha < NOW() group by nombre  order by total_vendido desc limit 1");
+    if (rows.length <= 0) {
+      return res.status(400).json({ message: "no encontrado" });
+    }
+    res.status(200).json(rows);
+  } catch (error) {
+    return res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+};
+
+export const getPorcentaje = async (req, res) => {
+  try {
+    // const id=req.params.id;
+    const [rows] = await pool.query("SELECT p.nombre AS nombre, SUM(dp.cantidad) AS total_vendido, SUM(dp.cantidad * dp.precio_unitario) AS Ingresos, ROUND((SUM(dp.cantidad * dp.precio_unitario) / (SELECT SUM(cantidad * precio_unitario) FROM detalle_de_pedidos)) * 100, 2) AS Porcentaje_Ingresos FROM detalle_de_pedidos dp INNER JOIN productos_en_venta p ON dp.id_producto = p.id_producto WHERE dp.fecha >= DATE_SUB(NOW(), INTERVAL 1 MONTH) AND dp.fecha < NOW() GROUP BY nombre ORDER BY total_vendido DESC;");
     if (rows.length <= 0) {
       return res.status(400).json({ message: "no encontrado" });
     }
--- src/routes/01_Estadisticas.routes.js
+++ src/routes/01_Estadisticas.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
 import { getLastMax } from "../controllers/01_Estadisticas.js";
+import { getOneMax } from "../controllers/01_Estadisticas.js";
+import { getPorcentaje } from "../controllers/01_Estadisticas.js";
 const router = Router();
 router.get('/estadisticas/lastmax', getLastMax); //listo
+router.get('/estadisticas/OneMax', getOneMax);
+router.get('/estadisticas/Porcentaje', getPorcentaje);
 export default router;
